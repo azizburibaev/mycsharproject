3ae87dc6e65f621c2246f6ad3e9af517d46dd36b
azizburibaev <azizburibaev@yahoo.com>
2022-07-02 22:46:26 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Project");
+            int [] myInt = {1,2, 3}; 
+            foreach (var item in myInt)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
